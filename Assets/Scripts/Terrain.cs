79709689606654df362425231c206e41419ac90c
Pablo <pablrohe@gmail.com>
2026-03-22 12:21:35 +0100

    CreateScripts

--- Assets/Scripts/Terrain.cs
+++ Assets/Scripts/Terrain.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 
-public class Terrain : MonoBehaviour
+public static class Terrain
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
     {
-        
-    }
+        float[,] noiseMap = new float[mapWidth, mapHeight];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (scale <= 0)
+        {
+            scale = 0.0001f; // Prevent division by zero
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float noiseValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = noiseValue;
+            }
+        }
+
+        return noiseMap;
     }
 }
---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public void GenerateTerrain()
+    {
+        float[,] noiseMap = Terrain.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        // Here you can use the noiseMap to create your terrain, for example by applying it to a mesh or a texture.
+        MapDisplay mapDisplay = FindAnyObjectByType<MapDisplay>();
+        mapDisplay.DrawNoiseMap(noiseMap);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
